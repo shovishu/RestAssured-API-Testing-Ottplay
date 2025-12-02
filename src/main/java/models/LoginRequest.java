3901e21e76d8ea89b46f7f2a3de826d3d912f0da
shovishu <shovishu@gmail.com>
2025-12-02 16:41:36 +0530

    Added more tests in TestNG file

---
+++ src/main/java/models/LoginRequest.java
@@ -0,0 +1,21 @@
+package models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * POJO representing the login request body.
+ * Lombok annotations remove boilerplate code like getters/setters.
+ */
+@Data                   // generates getters, setters, toString, equals, hashCode
+@Builder                // enables builder pattern
+@NoArgsConstructor      // no-args constructor (needed by Jackson)
+@AllArgsConstructor     // all-args constructor
+public class LoginRequest {
+    private String cellNumber;
+    private String password;
+    private String referrer;
+    private boolean newsletterConsent;
+}
